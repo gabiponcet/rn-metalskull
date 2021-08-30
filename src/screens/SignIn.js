b48a6adbfe9fd33da699f85c4035583427adf650
Gabi Poncet <gabiponcet@gmail.com>
2021-08-30 20:49:35 -0300

    add serviço firebase

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   View,
@@ -10,14 +10,28 @@ import {
 } from 'react-native';
 import Button from '../components/Button';
 import {COLORS} from '../assets/colors';
+import auth from '@react-native-firebase/auth';
 
 const SingIn = props => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+
   const recoverPassword = () => {
     alert('Recuperar senha');
   };
 
   const login = () => {
-    alert('Logar no sistema');
+    console.log(`${email} ${pass}`);
+    auth()
+      .signInWithEmailAndPassword(email, pass)
+      .then(() => {
+        alert('Logou');
+        setEmail('');
+        setPass('');
+      })
+      .catch(e => {
+        console.log('SignIn: erro ao fazer login' + e);
+      });
   };
 
   const cadastrar = () => {
@@ -33,8 +47,24 @@ const SingIn = props => {
             source={require('../assets/images/base.png')}
             accessibilityLabel="logo MetalSkull"
           />
-          <TextInput style={styles.input} />
-          <TextInput style={styles.input} />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            keyboardType="email.address"
+            returnKeyType="next"
+            onChangeText={t => setEmail(t)}
+            onEndEditing={() => this.passTextInput.focus()}
+          />
+          <TextInput
+           /*  ref={ref => {
+              this.passTextInput = ref;
+            }} */
+            style={styles.input}
+            placeholder="Senha"
+            keyboardType="default"
+            returnKeyType="go"
+            onChangeText={t => setPass(t)}
+          />
           <Text style={styles.forgotPassword} onPress={recoverPassword}>
             Esqueci minha senha
           </Text>
